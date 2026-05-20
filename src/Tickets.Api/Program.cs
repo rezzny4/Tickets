361313158ebdb43e0a1bac269f65587244fd8625
rezzny4 <ivan.burgard@gmail.com>
2026-05-21 01:21:56 +0200

    Made swagger only for dev

--- src/Tickets.Api/Program.cs
+++ src/Tickets.Api/Program.cs
@@ -42,8 +42,11 @@ var app = builder.Build();
 
 app.UseExceptionHandler();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapWolverineEndpoints();
 
